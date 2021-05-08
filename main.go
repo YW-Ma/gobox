de6f54f0e6a0529ec7d4b10eed90ca97fd35dea7
YW-Ma <Ma_Yaowei@outlook.com>
2021-05-09 01:17:24 +0800

    feat: add miao

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/YW-Ma/gobox/biz"
 )
 
 func main() {
-	fmt.Println(biz.Sum([]int{5,4,3,2,1}))
+	fmt.Println(biz.Sum([]int{5, 4, 3, 2, 1}))
+	fmt.Println("miao")
 }
